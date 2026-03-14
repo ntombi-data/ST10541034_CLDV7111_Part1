53147d90258507132ebfbc1006ca4ae6dfc177cb
Ntombifuthi Dhludhlu <ndhludhlu@clientele.co.za>
2026-03-14 11:37:04 +0200

    Add project files.

---
+++ ST10541034_CLDV7111_Part1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10541034_CLDV7111_Part1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
